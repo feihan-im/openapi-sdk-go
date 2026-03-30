39162422e31589742e3e3915b2c2d0678b75c1f4
xalanq <xalanq@gmail.com>
2026-03-30 08:49:19 +0800

    upd(common): update SPDX-License-Identifier

--- core/consts.go
+++ core/consts.go
@@ -1,3 +1,6 @@
+// Copyright (c) 2026 上海飞函安全科技有限公司 (Shanghai Feihan Security Technology Co., Ltd.)
+// SPDX-License-Identifier: Apache-2.0
+
 package fhcore
 
 const (
--- core/version.go
+++ core/version.go
@@ -1,7 +1,10 @@
+// Copyright (c) 2026 上海飞函安全科技有限公司 (Shanghai Feihan Security Technology Co., Ltd.)
+// SPDX-License-Identifier: Apache-2.0
+
 package fhcore
 
 // SDK version
-const Version = "0.28.100"
+const Version = "0.28.101"
 
 // User-Agent
 const UserAgent = "feihan-openapi-sdk-go/" + Version
